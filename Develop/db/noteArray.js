69ab3601a3f1ea551b54562570036bfbf93f3480
Haymanot W <haymanotalemu21@gmail.com>
2020-08-06 23:15:56 -0400

    assigning a unique id for each new notes posted

--- Develop/db/noteArray.js
+++
@@ -1,7 +0,0 @@
-var noteArray = [{
-    id: 1,
-    title:"Test Title",text:"Test text"}];
-
-
-//Note how we export the array. This makes it accessible to other files using require.
-module.exports = noteArray;
--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,8 +1,9 @@
+const fs = require("fs");
+const { json } = require("express");
 // LOAD DATA
 // We are linking our routes to a series of "data" sources.
 
-var noteData = require("../db/noteArray");
-
+var noteData = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
 module.exports = function(app) {
     // API GET Requests
@@ -13,22 +14,29 @@ module.exports = function(app) {
     app.get("/api/notes", function(req, res){
         res.json(noteData);
     });
+
+    // Below code handles when a user request a specific resource
+    app.get("/api/notes/:id", function(req, res){
+        res.json(noteData[Number(req.params.id)]);
+    });
+    // ========================================================= //
     // API POST Requests
   // Below code handles when a user saves a notes and thus store data to the server.
     app.post("/api/notes", function(req, res){
         // req.body hosts is equal to the JSON post sent from the user
         // This works because of our body parsing middleware
-        const newNote ={
-            id: noteData.length + 1,
-            title: req.body.title,
-            text: req.body.text
-        };
+        let newNote = req.body;
+        // Here we give a way to assign a unique id to our new notes posted by the user.
+        let uniqueId = (noteData.length).tostring();
+        newNote.id = uniqueId;
         noteData.push(newNote);
-        res.json(newNote);
+        
+        res.json(noteData);
 
     });
 
-    //Delete request
+    //Delete a specific resource 
+    // Dynamic route
     app.delete("/api/notes/:id", function(req, res){
         const note = noteData.find(n => n.id === parseInt(req.params.id));
         if(!note) res.status(404).send('The note with a given ID is not found');
